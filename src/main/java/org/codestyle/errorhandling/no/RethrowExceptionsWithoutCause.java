55f7dfa3459bec2133e94238ac6cb37eb3a7aa87
Rupert Smith <rupertgithub@badgerhouse.freeserve.co.uk>
2015-03-24 11:10:23 +0000

    Added rethrow without cause to no.

---
+++ src/main/java/org/codestyle/errorhandling/no/RethrowExceptionsWithoutCause.java
@@ -0,0 +1,32 @@
+package org.codestyle.errorhandling.no;
+
+import org.codestyle.errorhandling.yes.UserException;
+
+public abstract class RethrowExceptionsWithoutCause {
+    public abstract void tryStuff() throws UserException;
+
+    /**
+     * When re-throwing an exception by wrapping it in another exception, the original exception must be set as the
+     * cause, otherwise its stack trace will be lost.
+     *
+     * @throws MoreAppropriateForThisLevelException A more appropriate exception for this layer of code.
+     */
+    public void rethrowExceptions() throws MoreAppropriateForThisLevelException {
+        try {
+            tryStuff();
+        } catch (UserException e) {
+            throw new IllegalStateException();
+        }
+
+        try {
+            tryStuff();
+        } catch (UserException e) {
+            throw new MoreAppropriateForThisLevelException();
+        }
+    }
+
+    private class MoreAppropriateForThisLevelException extends Exception {
+        public MoreAppropriateForThisLevelException() {
+        }
+    }
+}
